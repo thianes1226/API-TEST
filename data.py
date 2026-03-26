17184dd866a8d950a5dfdad3b3eacb50706a84db
thianes1226 <thianesarr1226@gmail.com>
2026-03-26 13:52:28 +0000

    tutogit

---
+++ data.py
@@ -0,0 +1,28 @@
+
+ ## Ma liste d'étudiants
+eleves = [
+    {
+        "nom": "Dupont",
+        "prenom": "Jean",
+        "age": 15,
+        "classe": "2nde A"
+    },
+    {
+        "nom": "Lefebvre",
+        "prenom": "Marie",
+        "age": 14,
+        "classe": "3eme B"
+    },
+    {
+        "nom": "Traoré",
+        "prenom": "Oumar",
+        "age": 16,
+        "classe": "1ere S"
+    },
+    {
+        "nom": "Moreau",
+        "prenom": "Camille",
+        "age": 15,
+        "classe": "2nde A"
+    }
+]
