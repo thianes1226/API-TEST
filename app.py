0affe1323c3171615a2e3e59d108305abcec6e92
thianes1226 <thianesarr1226@gmail.com>
2026-04-08 12:38:26 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import psycopg2
 
+
 app = Flask(__name__)
 
 ## Connexion à la base de données
@@ -17,7 +18,8 @@ def get_db_connection():
 def hello():
     return "Hello, thiane ! bienvenue dans mon API Flask"
 
-## tous les personnes
+
+## récupérer toutes les personnes
 @app.route('/people/person')
 def get_people():
     conn = get_db_connection()
@@ -44,10 +46,8 @@ def get_person_by_id(id):
     cur = conn.cursor()
     cur.execute("SELECT * FROM person WHERE id = %s;", (id,))
     row = cur.fetchone()
-
     cur.close()
     conn.close()
-
     if row:
         return jsonify({
             "id": row[0],
@@ -55,11 +55,12 @@ def get_person_by_id(id):
             "fname": row[2],
             "timestamp": str(row[3])
         })
+    
     else:
         return jsonify({"message": "Person non trouvée"}), 404
 
 ## ajouter une personne
-@app.route('/people/person/add', methods=['GET'])
+@app.route('/people/person/add')
 def add_person_get():
     lname = request.args.get('lname')
     fname = request.args.get('fname')
@@ -76,8 +77,29 @@ def add_person_get():
     cur.close()
     conn.close()
 
-    return jsonify({"message": "Person ajoutée avec GET"})
+    return jsonify({"message": "Person ajoutée avec succès"}), 201
+  
+ ## supprimer une personne par son id
+@app.route('/people/person/delete/<int:id>')
+def delete_person(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
 
+    cur.execute("SELECT * FROM person WHERE id = %s;", (id,))
+    person = cur.fetchone()
+
+    if person is None:
+        cur.close()
+        conn.close()
+        return jsonify({"error": "Personne non trouvée"}), 404
+
+    cur.execute("DELETE FROM person WHERE id = %s;", (id,))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return jsonify({"message": "Person supprimée avec succès"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
